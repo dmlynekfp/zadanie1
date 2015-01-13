111a8d3644684f71a349463fd66f513404ab210c
Dominik Młynek <dmlynek@FP-PC2742.fp.lan>
2015-01-13 13:17:53 +0100

    scripts

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,11 +1,25 @@
 $(document).ready(function(){
-    judge.validate(document.getElementById('team_name'), {
-        valid: function(element) {
-            element.style.border = '1px solid green';
-        },
-        invalid: function(element, messages) {
-            element.style.border = '1px solid red';
-            alert(messages.join(','));
-        }
-    });
+
+
+    function validate()
+    {
+        judge.validate(document.getElementById('team_name'), {
+            valid: function(element) {
+                element.style.border = '1px solid green';
+            },
+            invalid: function(element, messages) {
+                element.style.border = '1px solid red';
+                //alert(messages.join(','));
+            }
+        });
+    }
+
+    validate();
+
+    $("#team_name").keyup(validate);
+
+
+
+
 });
+
